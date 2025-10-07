621c8221660057a4759dbd0570a00cf8f09c9512
cjcocokrisp <chriscoco1205@gmail.com>
2025-10-07 17:20:02 -0400

    feat(ui): add daisyui theme for project & update logo color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,22 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        t1dash: {
+          "primary": "#E63946",
+          "secondary": "#ffb703",
+          "accent": "#f77f00",
+          "neutral": "#ffffff",
+          "base-100": "#ffffff",
+          "info": "#e63946",
+          "success": "#4cAf50",
+          "warning": "#00ff00",
+          "error": "#ff0000",
+        },
+      },
+    ],
+  },
 }
 
